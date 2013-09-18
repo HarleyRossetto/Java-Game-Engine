856721103d9db9371dec8ebe818a0b56189ea948
GleamingShadow <harleyxr@optusnet.com.au>
2013-09-18 18:43:16 +1000

    Added generic methods for texture map cell identification and added 3D text rendering.

--- src/au/com/esm/eloi/src/RendererText.java
+++ src/au/com/esm/eloi/src/RendererText.java
@@ -14,22 +14,38 @@ public class RendererText {
 	}
 	
 	public void drawString(String string, float xPosition, float yPosition){
-		this.drawString(string, xPosition, yPosition, 0f, 0f, 0f, 0.5f);
+		this.drawString(string, xPosition, yPosition, 0F, 0F, 0F, 0.5F);
 	}
 	
 	public void drawString(String string, float xPosition, float yPosition, float alpha){
-		this.drawString(string, xPosition, yPosition, 0f, 0f, 0f, alpha);
+		this.drawString(string, xPosition, yPosition, 0F, 0F, 0F, alpha);
 	}
 	
 	public void drawString(String string, float xPosition, float yPosition, float red, float green, float blue){
-		this.drawStringInternal(string, xPosition, yPosition, red, green, blue, 1.0F);
+		this.drawStringInternal2D(string, xPosition, yPosition, red, green, blue, 1.0F);
 	}
 	
 	public void drawString(String string, float xPosition, float yPosition, float red, float green, float blue, float alpha){
-		this.drawStringInternal(string, xPosition, yPosition, red, green, blue, alpha);
+		this.drawStringInternal2D(string, xPosition, yPosition, red, green, blue, alpha);
 	}
 	
-	private void drawStringInternal(String string, float xPosition, float yPosition, float red, float green, float blue, float alpha){
+	public void drawString3D(String string, float xPosition, float yPosition, float zPosition){
+		this.drawStringInternal3D(string, xPosition, yPosition, zPosition, 0F, 0F, 0F, 0F, 0F, 0F, 0.5F);
+	}
+	
+	public void drawString3D(String string, float xPosition, float yPosition, float zPosition, float red, float green, float blue){
+		this.drawStringInternal3D(string, xPosition, yPosition, zPosition, 0, 0, 0, red, green, blue, 1.0F);
+	}
+	
+	public void drawString3D(String string, float xPosition, float yPosition, float zPosition, float red, float green, float blue, float alpha){
+		this.drawStringInternal3D(string, xPosition, yPosition, zPosition, 0F, 0F, 0F, red, green, blue, alpha);
+	}
+	
+	public void drawString3D(String string, float xPosition, float yPosition, float zPosition, float pitch, float yaw, float roll, float red, float green, float blue, float alpha){
+		this.drawStringInternal3D(string, xPosition, yPosition, zPosition, yaw, pitch, roll, red, green, blue, alpha);
+	}	
+	
+	private void drawStringInternal2D(String string, float xPosition, float yPosition, float red, float green, float blue, float alpha){
 		theEloi.renderEngine.setRenderMode(EnumRenderMode.ORTHOGRAPHICAL);
 		char[] characters = string.toCharArray();
 		int mapValue;
@@ -37,23 +53,45 @@ public class RendererText {
 		theEloi.renderEngine.bindTexture("Font");
 		Constructor var1 = Constructor.instance;
 		for (int i = 0; i < characters.length; i++){
-			mapValue = (int)(characters[i]) - 65;
-			if (mapValue == -33){
-			} else if (mapValue > -31 && mapValue < 0){
-				mapValue += 58;
-			}
+			mapValue = getFontMapValue(characters[i]);
 			uv = RenderEngine.getTextureMap(1).getUVCoordinates(mapValue);
-			drawCharacter(var1, xPosition, yPosition, 0, uv, red, green, blue, alpha);
+			drawCharacter2D(var1, xPosition, yPosition, uv, red, green, blue, alpha);
 			xPosition += 16f;
 		}	
 		theEloi.renderEngine.setRenderMode(EnumRenderMode.PERSPECTIVE);
 	}
 	
-	private void drawCharacter(Constructor constructor, float x, float y, float z, TextureUV uv, float red, float green, float blue, float alpha){
-		GL11.glDisable(GL11.GL_DEPTH_TEST);
-		GL11.glDisable(GL11.GL_CULL_FACE);
-		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glEnable(GL11.GL_TEXTURE_2D);		
+	private void drawStringInternal3D(String string, float xPosition, float yPosition, float zPosition, float yaw, float pitch, float roll, float red, float green, float blue, float alpha){
+		theEloi.renderEngine.setRenderMode(EnumRenderMode.PERSPECTIVE);
+		char[] characters = string.toCharArray();
+		int mapValue;
+		TextureUV uv;
+		theEloi.renderEngine.bindTexture("Font");
+		Constructor var1 = Constructor.instance;
+		GL11.glPushMatrix();
+		GL11.glRotatef(pitch, 1, 0, 0);
+		GL11.glRotatef(yaw, 0, 1, 0);
+		GL11.glRotatef(roll, 0, 0, 1);
+		for (int i = 0; i < characters.length; i++){
+			mapValue = getFontMapValue(characters[i]);
+			uv = RenderEngine.getTextureMap(1).getUVCoordinates(mapValue);
+			drawCharacter3D(var1, xPosition, yPosition, zPosition, uv, red, green, blue, alpha);
+			xPosition += 16f;
+		}
+		GL11.glPopMatrix();
+	}
+	
+	private int getFontMapValue(char character){
+		int mapValue = (int)character - 65;
+		if (mapValue == -33){
+		} else if (mapValue > -31 && mapValue < 0){
+			mapValue += 58;
+		}
+		return mapValue;
+	}
+	
+	private void drawCharacter2D(Constructor constructor, float x, float y, TextureUV uv, float red, float green, float blue, float alpha){
+		this.preCharacterRender();
 		GL11.glBegin(GL11.GL_QUADS);
 		GL11.glColor4f(red, green, blue, alpha);
 		GL11.glTexCoord2f(uv.uMin, uv.vMax);
@@ -65,6 +103,26 @@ public class RendererText {
 		GL11.glTexCoord2f(uv.uMin, uv.vMin);
 		GL11.glVertex2f(x, y + squareSize);
 		GL11.glEnd();
+		this.postCharacterRender();
+	}
+	
+	private void drawCharacter3D(Constructor constructor, float x, float y, float z, TextureUV uv, float red, float green, float blue, float alpha){
+		this.preCharacterRender();
+		constructor.addVertexAndUV(x, y, z, uv.uMin, uv.vMax);
+		constructor.addVertexAndUV(x, y, z, uv.uMax, uv.vMax);
+		constructor.addVertexAndUV(x, y, z, uv.uMin, uv.vMin);
+		constructor.addVertexAndUV(x, y, z, uv.uMin, uv.vMin);
+		this.postCharacterRender();
+	}
+	
+	private void preCharacterRender(){
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		GL11.glDisable(GL11.GL_CULL_FACE);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);				
+	}
+	
+	private void postCharacterRender(){
 		GL11.glDisable(GL11.GL_BLEND);
 		GL11.glDisable(GL11.GL_TEXTURE_2D);
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
